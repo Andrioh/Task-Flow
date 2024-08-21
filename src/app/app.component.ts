337dc0effe98c420924acd02e2996a077a605f9d
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-21 04:34:14 -0300

    CSS - component header and account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Component/header/header.component";
+import { AccountComponent } from "./Component/account/account.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccountComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
