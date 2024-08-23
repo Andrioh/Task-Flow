35aa8f4365157cf8af7ae5ee6f05902c0cfddd4a
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-23 06:50:27 -0300

    css - new task

--- src/app/Page/index/index.component.ts
+++ src/app/Page/index/index.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../../Component/header/header.component";
 import { AccountComponent } from "../../Component/account/account.component";
 import { IndexcomponentComponent } from "../../Component/indexcomponent/indexcomponent.component";
 import { MyTaskComponent } from "../../Component/my-task/my-task.component";
+import { NewTaskComponent } from "../../Component/new-task/new-task.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent, AccountComponent, IndexcomponentComponent, MyTaskComponent],
+  imports: [HeaderComponent, AccountComponent, IndexcomponentComponent, MyTaskComponent, NewTaskComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
