a92f56f10d04ab3bf1e1b4b40d4545ddfcebd127
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-23 06:18:19 -0300

    css - mytask

--- src/app/Page/index/index.component.ts
+++ src/app/Page/index/index.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../Component/header/header.component";
 import { AccountComponent } from "../../Component/account/account.component";
 import { IndexcomponentComponent } from "../../Component/indexcomponent/indexcomponent.component";
+import { MyTaskComponent } from "../../Component/my-task/my-task.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent, AccountComponent, IndexcomponentComponent],
+  imports: [HeaderComponent, AccountComponent, IndexcomponentComponent, MyTaskComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
